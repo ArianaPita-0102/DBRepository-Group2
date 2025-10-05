cc0a18e0cc02fcde6af31a28f56fd49a9a155e2d
unknown <ariana.pita@ucb.edu.bo>
2025-10-05 18:30:44 -0400

    feat: Add Guest Model

--- Models/Guest.cs
+++ Models/Guest.cs
@@ -2,5 +2,11 @@
 {
     public class Guest
     {
+        public Guid Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+
+        public bool Confirmed { get; set; }
     }
 }
+
+
