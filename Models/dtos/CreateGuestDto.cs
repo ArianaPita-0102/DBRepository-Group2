552d6bf0e5fff971e2e0ca66eeee946cb8b03377
unknown <ariana.pita@ucb.edu.bo>
2025-10-05 18:37:04 -0400

    feat: Add Create Guest Dto

---
+++ Models/dtos/CreateGuestDto.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DBRepository_Group2.Models.dtos
+{
+    public record CreateGuestDto
+    {
+        public string FullName { get; set; } = string.Empty;
+        public bool Confirmed { get; set; }
+    }
+}
+
+
