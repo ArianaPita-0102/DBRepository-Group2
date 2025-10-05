0562521358071e81c5f9c3882724b9260a316f64
unknown <ariana.pita@ucb.edu.bo>
2025-10-05 19:21:48 -0400

    feat: Add IGuestService

---
+++ Services/IGuestService.cs
@@ -0,0 +1,13 @@
+﻿using DBRepository_Group2.Models;
+using DBRepository_Group2.Models.dtos;
+
+namespace DBRepository_Group2.Services
+{
+    public interface IGuestService
+    {
+        Task<Guest> Create(CreateGuestDto dto);
+        Task<bool> Delete(Guid id);
+        Task<IEnumerable<Guest>> GetAll();
+        Task<Guest?> GetById(Guid id);
+    }
+}
