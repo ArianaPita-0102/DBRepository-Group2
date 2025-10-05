cda3da3ef3215be2b6e6a816e82c2814324fa06a
unknown <ariana.pita@ucb.edu.bo>
2025-10-05 19:38:28 -0400

    feat: Add IGuestRepository

---
+++ Repositories/IGuestRepository.cs
@@ -0,0 +1,13 @@
+﻿using DBRepository_Group2.Models;
+
+namespace DBRepository_Group2.Repositories
+{
+    public interface IGuestRepository
+    {
+        Task<IEnumerable<Guest>> GetAll();
+        Task<Guest?> GetById(Guid id);
+        Task Add(Guest guest);
+        Task Delete(Guid id);
+    }
+}
+
