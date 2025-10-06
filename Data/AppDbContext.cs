b6e88bc76391d8aeb0255aa158826bd6f056b320
unknown <ariana.pita@ucb.edu.bo>
2025-10-05 20:53:57 -0400

    feat: Add Data

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using DBRepository_Group2.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DBRepository_Group2.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Guest> Guests => Set<Guest>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Guest>(b =>
+            {
+                b.HasKey(x => x.Id);
+                b.Property(x => x.FullName).IsRequired();
+                b.Property(x => x.Confirmed).IsRequired();
+            });
+        }
+    }
+}
