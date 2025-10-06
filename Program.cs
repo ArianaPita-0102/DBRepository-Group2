37a95e36ae2f0f2a7e66eacb17376675f7966e90
unknown <ariana.pita@ucb.edu.bo>
2025-10-06 11:51:42 -0400

    feat: configure DbContext with PostgreSQL connection

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,13 +8,25 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<DBRepository_Group2.Data.AppDbContext>(opt =>
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+
+
+//scoped solo para db externo y singleton para db en memoria
+
+builder.Services.AddScoped<DBRepository_Group2.Repositories.IGuestRepository, DBRepository_Group2.Repositories.GuestRepository>();
+builder.Services.AddScoped<DBRepository_Group2.Services.IGuestService, DBRepository_Group2.Services.GuestService>();
+
 var app = builder.Build();
+// Dependency Injection
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
 
 app.UseHttpsRedirection();
 
@@ -21,3 +35,5 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
