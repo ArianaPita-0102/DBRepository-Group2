c89552221e8e49407eb8dd5ca63175580c4d3df8
JMjuanma <juanma.soruco@gmail.com>
2025-10-06 11:22:46 -0400

    Feat:Dtos

---
+++ Models/Dtos/CreateEvent.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DBRepository_Group2.Models.Dtos
+{
+    public class CreateEvent
+    {
+        [Required, StringLength(200)]
+        public string Title { get; init; } = string.Empty;
+
+        [Range(1000, 2100)]
+        public int Year { get; init; }
+    }
+}
---
+++ Models/Event.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace DBRepository_Group2
+{
+        public class Event
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; } = string.Empty;
+            public DateTime Date { get; set; }
+            public int Capacity { get; set; }
+        }
+}
